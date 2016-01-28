9143d92850dfe03e2a9fd219df53c996a83bde1a
Erick Rivas <rivadunga@hotmail.com>
2016-01-28 10:50:02 -0600

    E2

--- ex2.c
+++ ex2.c
@@ -36,15 +36,58 @@ void forwardIt(void* datos, size_t size, int len, t_imprime imprime)
     }
 }
 
+void prevIt(void* datos, size_t size, int len, t_imprime imprime)
+{
+    void* temp = end(datos,size,len);
+    while(temp != begin(datos)){
+        imprime(temp);
+        temp = prev(temp,size);
+    }
+}
+
+void bidIt(void* datos, size_t size, int len, t_imprime imprime)
+{
+    void* b = begin(datos);
+    void* temp;
+    if (datos = b){
+        temp = begin(datos);
+        while(temp != end(datos,size,len)){
+            imprime(temp);
+            temp = next(temp,size);
+        }
+    }
+    else{
+        temp = end(datos,size,len);
+        while(temp != begin(datos)){
+            imprime(temp);
+            temp = prev(temp,size);
+        }
+    }
+}
+
 void imprimeChar(char* dato)
 {
     printf("%c",*dato);
+}
+
+void imprimeLibro(Libro* libro)
+{
+    printf("%c\n",libro->titulo);
 }
 
 int main(int argc, const char * argv[])
 {
     char* datos = "Hola q ace";
     forwardIt(datos,sizeof(char),10,&imprimeChar);
+    printf("\n");
+    prevIt(datos,sizeof(char),10,&imprimeChar);
+    printf("\n");
+
+    Libro* libros = (Libro*) malloc(3*sizeof(Libro));
+    libros->titulo = "hOLI";
+    (libros+1)->titulo = "hOLI2";
+    (libros+2)->titulo = "hOLI3";
+    bidIt(datos,sizeof(Libro),3,&imprimeLibro);
 }
 
 
